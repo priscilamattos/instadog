4fece1aabd55763b289cde752a5b7a9fa6a0c4c8
priscilamattos <priiica12@gmail.com>
2022-11-29 22:24:45 -0500

    useFetch

--- src/components/login/LoginCreate.jsx
+++ src/components/login/LoginCreate.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { USER_POST } from "../../api";
+import useFetch from "../../hooks/useFetch";
 import useForm from "../../hooks/useForm";
 import { UserContext } from "../../UserContext";
 import Button from "../forms/Button";
 import Input from "../forms/Input";
+import Error from "../Helper/Error";
 
 const LoginCreate = () => {
   const username = useForm();
@@ -12,6 +14,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -20,7 +23,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -31,7 +34,8 @@ const LoginCreate = () => {
         <Input label="Username" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Password" type="password" name="password" {...password} />
-        <Button>Sign Up</Button>
+        {loading ? <Button>Signing Up</Button> : <Button>Sign Up</Button>}
+        <Error error={error} />
       </form>
     </section>
   );
