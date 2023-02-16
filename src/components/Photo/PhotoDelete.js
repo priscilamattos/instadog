2c04a5982dcbb9e71b548ba13b8cd1893ccb0a72
priscilamattos <priiica12@gmail.com>
2023-02-15 23:44:00 -0500

    New: PhotoDelete.jsx

--- src/components/Photo/PhotoDelete.js
+++ src/components/Photo/PhotoDelete.js
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from "./PhotoDelete.module.css";
-import { PHOTO_DELETE } from "../../Api";
-import useFetch from "../../Hooks/useFetch";
+
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../hooks/useFetch";
+import styles from "./../../css/PhotoDelete.module.css";
 
 const PhotoDelete = ({ id }) => {
   const { loading, request } = useFetch();
 
   async function handleClick() {
-    const confirm = window.confirm("Tem certeza que deseja deletar?");
+    const confirm = window.confirm("Are you sure you want to delete?");
     if (confirm) {
       const { url, options } = PHOTO_DELETE(id);
       const { response } = await request(url, options);
@@ -23,7 +24,7 @@ const PhotoDelete = ({ id }) => {
         </button>
       ) : (
         <button onClick={handleClick} className={styles.delete}>
-          Deletar
+          Delete
         </button>
       )}
     </>
